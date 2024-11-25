cd191e14a1da9d67971b1f48d35bf9e99ecade87
ravenspeak <89217966+ravenspeak@users.noreply.github.com>
2024-11-25 23:48:19 +0000

    Update vzbackup-rclone.sh

--- vzbackup-rclone.sh
+++ vzbackup-rclone.sh
@@ -82,7 +82,7 @@ if [[ ${COMMAND} == 'job-end' ||  ${COMMAND} == 'job-abort' ]]; then
 
     # clean up remote backup location, all files more than 2d are deleted
     rclone --config /root/.config/rclone/rclone.conf \
-    --min-age 2d  delete rclonegdrive:/vzdump --rmdirs
+    --min-age 2d delete rclonegdrive:/vzdump --rmdirs
     
     #rm -rfv $rcloneroot
 fi
